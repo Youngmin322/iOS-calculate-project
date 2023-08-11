37dc094e15d49370a3a4a489dd245e84a25c85b9
youngmin <choyoungmin0322@gmail.com>
2023-08-11 10:56:49 +0900

    Initial Commit

---
+++ Calculate/CalculateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateApp.swift
+//  Calculate
+//
+//  Created by 조영민 on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct CalculateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
